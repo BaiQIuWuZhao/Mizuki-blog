6cc247fbdfaef5facca748b91b15d34a99e42b13
BaiQiuWuZhao <3276192042@qq.com>
2026-02-28 21:45:29 +0800

    Upgrade

--- src/data/devices.ts
+++ src/data/devices.ts
@@ -16,24 +16,24 @@ export type DeviceCategory = {
 };
 
 export const devicesData: DeviceCategory = {
-	OnePlus: [
+	手机: [
 		{
-			name: "OnePlus 13T",
-			image: "/images/device/oneplus13t.png",
-			specs: "Gray / 16G + 1TB",
+			name: "Sony Xperia 1 II(docomo)",
+			image: "/images/device/Sony Xperia 1 II.webp",
+			specs: "Black / 6G + 128GB",
 			description:
-				"Flagship performance, Hasselblad imaging, 80W SuperVOOC.",
-			link: "https://www.oneplus.com/cn/13t",
+				"始于颜值，忠于才华。",
+			link: "https://baike.baidu.com/item/索尼Xperia%201%20II/49897171",
 		},
 	],
-	Router: [
+	笔记本: [
 		{
-			name: "GL-MT3000",
-			image: "/images/device/mt3000.png",
-			specs: "1000Mbps / 2.5G",
+			name: "华为MateBook D15 R5版",
+			image: "/images/device/HUAWEI Matebook D15.webp",
+			specs: "5G / 2.38GHz",
 			description:
-				"Portable WiFi 6 router suitable for business trips and home use.",
-			link: "https://www.gl-inet.cn/products/gl-mt3000/",
+				"一款轻型办公本，不适合游戏",
+			link: "https://www.vmall.com/product/10086440610959.html",
 		},
 	],
 };
